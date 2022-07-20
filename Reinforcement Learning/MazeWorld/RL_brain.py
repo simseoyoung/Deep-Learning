9c2a7a3ecbbfbdb563c01b06a2c634b29406769e
simseoyoung <88702793+simseoyoung@users.noreply.github.com>
2022-07-20 16:36:30 -0400

    Add files via upload

---
+++ Reinforcement Learning/MazeWorld/RL_brain.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import pandas as pd
+
+
+class rlalgorithm:
+    def __init__(self, actions, *args, **kwargs):
+        """Your code goes here"""
+        pass
+
+    def choose_action(self, observation):
+        """Your code goes here"""
+        pass
+
+    def learn(self, s, a, r, s_):
+        """Your code goes here"""
+        pass
